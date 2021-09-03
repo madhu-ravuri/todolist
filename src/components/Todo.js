be2ee84c8369392c6cdbfcf58fcaac41d6a5f9cb
Madhu Ravuri <mr06mr@gmail.com>
2021-09-02 19:51:21 -0700

    fixing edit function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,6 +7,8 @@ const Todo = () => {
   const [create, showCreate] = useState(false);
   const [task, setTask] = useState("");
   const [tasks, setTasks] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [input, setInput] = useState("");
 
   const Button = styled.button`
     color: white;
@@ -44,6 +46,27 @@ const Todo = () => {
     setTask("");
   };
 
+  const handleEdit = (id) => {
+    console.log(id);
+    setEdit(id);
+  };
+
+  const editTask = (e, id, input) => {
+    e.preventDefault();
+
+    let changes = tasks.map((task) => {
+      if (task.id === id) {
+        setTask(input);
+      }
+      return task;
+    });
+    console.log(id);
+    console.log(task.id);
+    console.log(changes);
+    // setTasks(changes);
+    // setEdit(false);
+  };
+
   const deleteTask = (delIndex) => {
     const newTasks = tasks.filter((task, i) => {
       return i !== delIndex;
@@ -75,9 +98,29 @@ const Todo = () => {
         {tasks.map((task, i) => {
           return (
             <div key={i}>
-              <span>{task}</span>
-              <FaPen />
-              <FaTrashAlt onClick={(e) => deleteTask(i)} />
+              {edit === i ? (
+                <div>
+                  <form onSubmit={(e) => editTask(e, i, input)}>
+                    <input
+                      type="text"
+                      placeholder={task}
+                      onChange={(e) => setInput(e.target.value)}
+                    />
+                    <Button type="submit" style={{ backgroundColor: "black" }}>
+                      Save
+                    </Button>
+                  </form>
+                </div>
+              ) : (
+                <div>
+                  <span>{task}</span>
+                  <FaPen onClick={(e) => handleEdit(i)} />
+                  <FaTrashAlt onClick={(e) => deleteTask(i)} />
+                </div>
+              )}
+              {/* <span>{task}</span>
+              <FaPen onClick={(e) => handleEdit(i)} />
+              <FaTrashAlt onClick={(e) => deleteTask(i)} /> */}
             </div>
           );
         })}
